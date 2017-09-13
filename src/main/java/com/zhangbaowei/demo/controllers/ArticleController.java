6a0e80a8014c502279a6912a337081cd8a7a2eef
zhangbaowei <zbw911@gmail.com>
2017-09-13 10:03:42 +0800

    播放视频功能

--- src/main/java/com/zhangbaowei/demo/controllers/ArticleController.java
+++ src/main/java/com/zhangbaowei/demo/controllers/ArticleController.java
@@ -4,34 +4,31 @@ import com.zhangbaowei.demo.model.TestModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
-    @RequestMapping({"/","/index"})
-    public String index(  Model model) {
+    @RequestMapping({"/", "/index"})
+    public String index(Model model) {
 //        model.addAttribute("username", username);
 
 
-        TestModel tm = new TestModel(){{
+        TestModel tm = new TestModel() {{
             setSex("男");
             setUserId(1);
             setUserName("zbw");
-setDiscript("用于测试的<b>加黑</b>的");
+            setDiscript("用于测试的<b>加黑</b>的");
         }};
 
-        model.addAttribute("tm",tm);
+        model.addAttribute("tm", tm);
 
         return "article/index";
     }
 
 
-    @RequestMapping({ "/v"})
-    public String video(  Model model) {
+    @RequestMapping({"/v"})
+    public String video(Model model) {
 //        model.addAttribute("username", username);
-
-
 
 
         return "article/video";
