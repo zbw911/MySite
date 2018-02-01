ffccc32e36d8c73b66a81d7d576dcdaa20e0ba2d
zhangbaowei <zbw911@gmail.com>
2018-02-01 13:26:22 +0800

    支持多端口

--- src/main/java/com/zhangbaowei/demo/DemoApplication.java
+++ src/main/java/com/zhangbaowei/demo/DemoApplication.java
@@ -2,16 +2,15 @@ package com.zhangbaowei.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(EmbeddedTomcatConfiguration.class)
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 
 }
